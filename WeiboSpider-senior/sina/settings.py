656e0839feb050f0e0027e5694b85f3e94c16156
Administrator <xyf_0704@sina.com>
2018-11-02 14:11:46 +0800

    添加日志处理记录

--- WeiboSpider-senior/sina/settings.py
+++ WeiboSpider-senior/sina/settings.py
@@ -1,9 +1,17 @@
 # -*- coding: utf-8 -*-
+import datetime
 
 BOT_NAME = 'sina'
 
 SPIDER_MODULES = ['sina.spiders']
 NEWSPIDER_MODULE = 'sina.spiders'
+
+to_day = datetime.datetime.now()
+log_file_path = "log/scrapy_{}_{}_{}.log".format(to_day.year,to_day.month,to_day.day)
+
+LOG_LEVEL = "INFO"
+LOG_FILE = log_file_path
+
 
 ROBOTSTXT_OBEY = False
 
@@ -15,7 +23,7 @@ DEFAULT_REQUEST_HEADERS = {
 
 CONCURRENT_REQUESTS = 16
 
-DOWNLOAD_DELAY = 1
+DOWNLOAD_DELAY = 0.5
 
 DOWNLOADER_MIDDLEWARES = {
     'weibo.middlewares.UserAgentMiddleware': None,
