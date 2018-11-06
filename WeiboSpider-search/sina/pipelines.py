2af4af951b5f12533eb760ff2d67ad1ffeef9e1e
Administrator <xyf_0704@sina.com>
2018-11-06 15:34:15 +0800

    search/添加转发

--- WeiboSpider-search/sina/pipelines.py
+++ WeiboSpider-search/sina/pipelines.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import pymongo
 from pymongo.errors import DuplicateKeyError
-from sina.items import RelationshipsItem, TweetsItem, InformationItem, CommentItem
+from sina.items import RelationshipsItem, TweetsItem, InformationItem, CommentItem, RepostItem
 from sina.settings import LOCAL_MONGO_HOST, LOCAL_MONGO_PORT, DB_NAME
 
 
@@ -9,21 +9,24 @@ class MongoDBPipeline(object):
     def __init__(self):
         client = pymongo.MongoClient(LOCAL_MONGO_HOST, LOCAL_MONGO_PORT)
         db = client[DB_NAME]
-        self.Information = db["Information"]
-        self.Tweets = db["Tweets"]
-        self.Comments = db["Comments"]
-        self.Relationships = db["Relationships"]
+        self.Information = db["Information_gongjiao"]
+        self.Tweets = db["Tweets_gongjiao"]
+        self.Comments = db["Comments_gongjiao"]
+       # self.Relationships = db["Relationships_gongjiao"]
+        self.Reposts = db["Reposts_gongjiao"]
 
     def process_item(self, item, spider):
         """ 判断item的类型，并作相应的处理，再入数据库 """
-        if isinstance(item, RelationshipsItem):
-            self.insert_item(self.Relationships, item)
-        elif isinstance(item, TweetsItem):
+        # if isinstance(item, RelationshipsItem):
+        #     self.insert_item(self.Relationships, item)
+        if isinstance(item, TweetsItem):
             self.insert_item(self.Tweets, item)
         elif isinstance(item, InformationItem):
             self.insert_item(self.Information, item)
         elif isinstance(item, CommentItem):
             self.insert_item(self.Comments, item)
+        elif isinstance(item, RepostItem):
+            self.insert_item(self.Reposts, item)
         return item
 
     @staticmethod
